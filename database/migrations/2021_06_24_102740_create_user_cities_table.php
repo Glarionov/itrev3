bf39785f0327015d6dcff6596a9b792fbba7dd40
Glarionov <artevy@mail.ru>
2021-06-24 17:58:44 +0300

    make it better

---
+++ database/migrations/2021_06_24_102740_create_user_cities_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_cities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_cities');
+    }
+}
